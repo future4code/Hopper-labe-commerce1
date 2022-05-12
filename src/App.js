58440968ab59cb785d1d981e4335b6fd82b64eb6
roberta005 <roberta.devs@hotmail.com>
2022-05-12 12:01:05 -0300

    Primeira versão estática ainda sem componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+import { Filtros } from './Components/Filtros';
+
+// ESTILIZAÇÃO COM STYLED COMPONENTS
+
+const Tela = styled.div `
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+`
+const FiltroCarrinhoArea = styled.div `
+ border: 1px solid black;
+ height: 30em;
+ width: 15em;
+ margin: 10px;
+ text-align: center;
+`
+const ProdutosArea = styled.section`
+  display:flex ;
+  flex-direction: column;
+`
+const ContainerSuperior = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
+`
+const ContainerProdutos = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+const Produto = styled.div`
+  border: 1px solid black;
+  width: 18em;
+  height: 25em;
+  font-size: 15px;
+  margin:2px;
+  img{
+  width: 18em;
+  height: 12em;
+}
+p{
+  margin: 1em;
+}
+button{
+  margin: 0 6em;
+}
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Tela>
+      <FiltroArea>
+        <Filtros/>
+      </FiltroArea>
+          <ProdutosArea>
+        <ContainerSuperior>
+          <p>Quantidade de produtos: </p>
+          <select> Ordenação
+            <option>Crescente</option>
+            <option>Decrescente</option></select>
+        </ContainerSuperior>
+        <ContainerProdutos>
+            <Produto>
+              <img src='https://neilpatel.com/wp-content/uploads/2019/07/mini-caixas-de-produtos-em-cima-de-laptop.jpeg'></img>
+              <p>Título</p>
+              <p>Preço</p>
+              <button>Adicionar ao carrinho</button>
+            </Produto>
+            <Produto>
+              <img src='https://neilpatel.com/wp-content/uploads/2019/07/mini-caixas-de-produtos-em-cima-de-laptop.jpeg'></img>
+              <p>Título</p>
+              <p>Preço</p>
+              <button>Adicionar ao carrinho</button>
+            </Produto>
+            
+        </ContainerProdutos>
+          </ProdutosArea>
+          <FiltroCarrinhoArea>
+            <h3> Carrinho:</h3>
+          </FiltroCarrinhoArea>
+    </Tela>
   );
 }
 
---
+++ src/Components/Filtros.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Filtros(){
+    return <div>
+    <h3> Filtros</h3>
+    <input placeholder="Preço Mínimo"/>
+    <input placeholder="Preço Máximo"/>
+    <input placeholder="Nome do produto"/>
+  </div>
+}
